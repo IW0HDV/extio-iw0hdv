153c8770bcafe35cf0006a375846452d75be875b
Andrea Montefusco IW0HDV <andrew@montefusco.com>
2015-01-25 18:19:00 +0100

    AirSpy: parameters persistence and numerical display added.

--- airspy/gui_control.cpp
+++ airspy/gui_control.cpp
@@ -33,26 +33,47 @@
 #include "gui.h" 
 #include "gui_control.h"
 #include "airspyw.h"
+//#include <string>
 
-
-AirSpyCtrlGui::AirSpyCtrlGui(ExtioAirSpyRadio < EXTIO_BASE_TYPE > *pr) : Gui(IDD_AIRSPY_CONTROL_DLG), pr_(pr)
+AirSpyCtrlGui::AirSpyCtrlGui(ExtioAirSpyRadio < EXTIO_BASE_TYPE > *pr) : Gui(IDD_AIRSPY_CONTROL_DLG), pr_(pr), cfg_(0)
 {
-	LOGT("********************************* AirSpyCtrlGui: pImpl: %p Gui addr: %p\n", pi, this);
+	std::string fn("AIRSPY");
+	
+	if (pr_) fn += pr_->get_serial();
+	
+	cfg_ = new Config<AIRSPY_CFG_T>((fn+".txt").c_str(), std::make_tuple(2500000, 5, 10, 6, 0, 0, 0));
+
+	LOGT("********************************* AirSpyCtrlGui: pImpl: %p Gui addr: %p Cfg: %p, (%s)\n", pi, this, cfg_, fn.c_str());
 
 	if (pi && pi->hDialog) OnInit(GuiEvent(pi->hDialog, -1));
 }
 
+AirSpyCtrlGui::~AirSpyCtrlGui () 
+{
+	delete cfg_;
+}
+
 bool AirSpyCtrlGui::OnInit(const GuiEvent& ev)
 {
-	LOGT("Event ref: %p\n", ev);
+	LOGT("Event ref: %p cfg: %p\n", ev, cfg_);
 
 	// sample rate combobox, there are only two sample rates available in AIRSPY receiver
 	SendMessage(GetDlgItem(ev.hWnd, ID_COMBO_SR), CB_ADDSTRING, 0, (LPARAM)"10000000");
 	SendMessage(GetDlgItem(ev.hWnd, ID_COMBO_SR), CB_ADDSTRING, 0, (LPARAM)"2500000");
-	if ((pr_) && pr_-> get_sample_rate() == 2500000)
+
+	int sample_rate = 2500000;
+	if (cfg_) {
+	    sample_rate = cfg_->get<C_SR,int>();
+	    LOGT("Sample rate from cfg(%p): %d\n", cfg_, sample_rate);
+	}
+
+	if (sample_rate == 2500000) {
         SendMessage(GetDlgItem(ev.hWnd, ID_COMBO_SR), CB_SETCURSEL, 1, 0);
-	else 
+		if (pr_) pr_-> set_sample_rate(sample_rate);
+    } else {
         SendMessage(GetDlgItem(ev.hWnd, ID_COMBO_SR), CB_SETCURSEL, 0, 0);
+    	if (pr_) pr_-> set_sample_rate(10000000);
+	}
 		
 	// 
 	// http://msdn.microsoft.com/en-us/library/windows/desktop/bb760238%28v=vs.85%29.aspx
@@ -60,34 +81,53 @@ bool AirSpyCtrlGui::OnInit(const GuiEvent& ev)
 	HWND sliderIfGain = GetDlgItem(ev.hWnd, IDS_IF_GAIN);
 	SendMessage(sliderIfGain, TBM_SETRANGE,    (WPARAM)TRUE, (LPARAM)MAKELONG(0, 15));
 	SendMessage(sliderIfGain, TBM_SETTICFREQ,  (WPARAM)1,  0);
-	int if_gain;
-	if (pr_) { 
-	    if_gain = pr_->get_vga_gain(); 
-        SendMessage(sliderIfGain, TBM_SETPOS, (WPARAM)TRUE, if_gain);
-    	DlgItemPrint(GetDlgItem(ev.hWnd, IDC_ST_IF_GAIN_VAL), "%d", if_gain);
-	}
+	int if_gain = cfg_->get<C_IF_G,int>();
+	if (pr_) pr_->set_vga_gain(if_gain); 
+	SendMessage(sliderIfGain, TBM_SETPOS, (WPARAM)TRUE, if_gain);
+    DlgItemPrint( GuiEvent (GetDlgItem(ev.hWnd, IDC_ST_IF_GAIN_VAL), 0), "%d", if_gain);
 	
 	HWND sliderMixerGain = GetDlgItem(ev.hWnd, IDS_MIXER_GAIN);
-	SendMessage(sliderMixerGain, TBM_SETRANGE, (WPARAM)TRUE, (LPARAM)MAKELONG(0, 15));
+	SendMessage(sliderMixerGain, TBM_SETRANGE,    (WPARAM)TRUE, (LPARAM)MAKELONG(0, 15));
 	SendMessage(sliderMixerGain, TBM_SETTICFREQ,  (WPARAM)1,  0);
-	int mixer_gain;
-	if (pr_) { 
-	    mixer_gain = pr_->get_mixer_gain(); 
-        SendMessage(sliderMixerGain, TBM_SETPOS, (WPARAM)TRUE, mixer_gain);
-	    DlgItemPrint(GetDlgItem(ev.hWnd, IDC_ST_MIXER_GAIN_VAL), "%d", mixer_gain);
-    }
-	
+	int mixer_gain = cfg_->get<C_MIX_G,int>();
+	if (pr_) pr_->set_mixer_gain(mixer_gain); 
+    SendMessage(sliderMixerGain, TBM_SETPOS,    (WPARAM)TRUE, mixer_gain);
+	DlgItemPrint( GuiEvent (GetDlgItem(ev.hWnd, IDC_ST_MIXER_GAIN_VAL), 0), "%d", mixer_gain);
+    
 	HWND sliderLnaGain = GetDlgItem(ev.hWnd, IDS_LNA_GAIN);
 	SendMessage(sliderLnaGain, TBM_SETRANGE,    (WPARAM)TRUE, (LPARAM)MAKELONG(0, 15));
 	SendMessage(sliderLnaGain, TBM_SETTICFREQ,  (WPARAM)1,  0);
-	int lna_gain;
-	if (pr_) { 
-	    lna_gain = pr_->get_lna_gain(); 
-        SendMessage(sliderLnaGain, TBM_SETPOS, (WPARAM)TRUE, lna_gain);
-	    DlgItemPrint(GetDlgItem(ev.hWnd, IDC_ST_LNA_GAIN_VAL), "%d", lna_gain);
-	}
+	int lna_gain = cfg_->get<C_LNA_G,int>();
+	if (pr_) pr_->set_lna_gain(lna_gain); 
+    SendMessage(sliderLnaGain, TBM_SETPOS, (WPARAM)TRUE, lna_gain);
+	DlgItemPrint( GuiEvent (GetDlgItem(ev.hWnd, IDC_ST_LNA_GAIN_VAL),0), "%d", lna_gain);
+	
+	// MIXER AGC check box
+	int mixer_agc = cfg_->get< C_A_MIX_G, int>();
+	LOGT("MIX AGC value from configuration: %d\n", mixer_agc);
+	if (pr_) pr_->set_mixer_agc (mixer_agc);
+    CheckDlgButton (ev.hWnd, IDCB_AUTO_MIXER_GAIN, mixer_agc);
+    // enable manual control accordingly
+	::EnableWindow(GetDlgItem(ev.hWnd, IDS_MIXER_GAIN), (mixer_agc == 0));		
+
+	// LNA Automatic Gain Control  check box
+	int lna_agc = cfg_->get< C_A_LNA_G, int>();
+	LOGT("LAN AGC value from configuration: %d\n", lna_agc);
+	if (pr_) pr_->set_lna_agc (lna_agc);
+    CheckDlgButton (ev.hWnd, IDCB_AUTO_LNA_GAIN, lna_agc);
+    // enable manual control accordingly
+    ::EnableWindow(GetDlgItem(ev.hWnd, IDS_LNA_GAIN), (lna_agc == 0));		
 
 	
+	// BIAS check box
+	int bias = cfg_->get< C_BIAS, int>();
+	LOGT("BIAS value from configuration: %d\n", bias);
+	if (pr_) pr_->set_rf_bias(bias);	
+    CheckDlgButton (ev.hWnd, IDCB_BIAS_TEE, bias);
+
+    //
+    // setup for tool bar
+    //	
 	HICON icon = LoadIcon(::GetMyHandle(), MAKEINTRESOURCE(IDI_ICON1));
 	
 	SendMessage(ev.hWnd, WM_SETICON, ICON_BIG, (LPARAM)icon);
@@ -117,15 +157,6 @@ void AirSpyCtrlGui::EnableControls()
 void AirSpyCtrlGui::DisableControls()
 {
 	GuiEvent ev(pi->hDialog, 0);
-#if 0
-	
-	EnableAll(ev, GuiEvent(0, false));
-	EnableAll(ev, GuiEvent(GetDlgItem(pi->hDialog, ID_CB_AMP), true));
-	EnableAll(ev, GuiEvent(GetDlgItem(pi->hDialog, ID_CB_DITHER), true));
-	EnableAll(ev, GuiEvent(GetDlgItem(pi->hDialog, ID_COMBO_ATT), true));
-	EnableAll(ev, GuiEvent(GetDlgItem(pi->hDialog, ID_COMBO_SR), true));
-#endif	
-
 	Gui::Show();
 }
 
@@ -137,6 +168,7 @@ bool AirSpyCtrlGui::ButtonClick(const GuiEvent &ev)
 	    int bias = (IsDlgButtonChecked(ev.hWnd, IDCB_BIAS_TEE) == BST_CHECKED) ? 1 : 0;
 		LOGT("New BIAS value: %d\r\n", bias);
 		if (pr_) pr_->set_rf_bias(bias);
+		cfg_->set< C_BIAS, int>(bias);
 	}
 
 	// MIXER GAIN Automatic Gain Control  check box
@@ -144,6 +176,7 @@ bool AirSpyCtrlGui::ButtonClick(const GuiEvent &ev)
 	    int mixer_agc = (IsDlgButtonChecked(ev.hWnd, IDCB_AUTO_MIXER_GAIN) == BST_CHECKED) ? 1 : 0;
 		LOGT("New IDCB_AUTO_MIXER_GAIN value: %d\r\n", mixer_agc);
 		if (pr_) pr_->set_mixer_agc (mixer_agc);
+		cfg_->set< C_A_MIX_G, int>(mixer_agc);
         // enable manual control accordingly
 		::EnableWindow(GetDlgItem(ev.hWnd, IDS_MIXER_GAIN), (mixer_agc == 0));		
 	}
@@ -153,33 +186,38 @@ bool AirSpyCtrlGui::ButtonClick(const GuiEvent &ev)
 	    int lna_agc = (IsDlgButtonChecked(ev.hWnd, IDCB_AUTO_LNA_GAIN) == BST_CHECKED) ? 1 : 0;
 		LOGT("New IDCB_AUTO_LNA_GAIN value: %d\r\n", lna_agc);
 		if (pr_) pr_->set_lna_agc (lna_agc);
+		cfg_->set< C_A_LNA_G, int>(lna_agc);
         // enable manual control accordingly
 		::EnableWindow(GetDlgItem(ev.hWnd, IDS_LNA_GAIN), (lna_agc == 0));		
 	}
 	
 	return true;
 }
+
 bool  AirSpyCtrlGui::OnHScroll(const GuiEventHScroll& ev)
 {
 	if (GetDlgItem(ev.hWnd, IDS_LNA_GAIN) == ev.hwndCtl) {
 		DWORD newPos = SendMessage(GetDlgItem(ev.hWnd, IDS_LNA_GAIN), TBM_GETPOS, 0, 0);
-		DlgItemPrint(GetDlgItem(ev.hWnd, IDC_ST_LNA_GAIN_VAL), "%d", newPos);
+		DlgItemPrint( GuiEvent (GetDlgItem(ev.hWnd, IDC_ST_LNA_GAIN_VAL), 0), "%d", newPos);
 		LOGT("New LNA GAIN value: %d\r\n", newPos);
 		if (pr_) pr_->set_lna_gain (newPos);
+		cfg_->set<C_LNA_G,int>(newPos);
 		return true;
 	} else
 	if (GetDlgItem(ev.hWnd, IDS_MIXER_GAIN) == ev.hwndCtl) {
 		DWORD newPos = SendMessage(GetDlgItem(ev.hWnd, IDS_MIXER_GAIN), TBM_GETPOS, 0, 0);
-		DlgItemPrint(GetDlgItem(ev.hWnd, IDC_ST_MIXER_GAIN_VAL), "%d", newPos);
+		DlgItemPrint( GuiEvent (GetDlgItem(ev.hWnd, IDC_ST_MIXER_GAIN_VAL), 0), "%d", newPos);
 		LOGT("New MIXER GAIN value: %d\r\n", newPos);
 		if (pr_) pr_->set_mixer_gain (newPos);
+		cfg_->set<C_MIX_G,int>(newPos);
 		return true;
 	} else
 	if (GetDlgItem(ev.hWnd, IDS_IF_GAIN) == ev.hwndCtl) {
 		DWORD newPos = SendMessage(GetDlgItem(ev.hWnd, IDS_IF_GAIN), TBM_GETPOS, 0, 0);
-		DlgItemPrint(GetDlgItem(ev.hWnd, IDC_ST_IF_GAIN_VAL), "%d", newPos);
+		DlgItemPrint( GuiEvent (GetDlgItem(ev.hWnd, IDC_ST_IF_GAIN_VAL), 0), "%d", newPos);
 		LOGT("New IF GAIN value: %d\r\n", newPos);
 		if (pr_) pr_->set_vga_gain (newPos);
+		cfg_->set<C_IF_G,int>(newPos);
 		return true;
 	} else
 		return false;
@@ -197,6 +235,7 @@ bool AirSpyCtrlGui::ComboBoxSelChange(const GuiEvent &ev)
 		
 		int nsr;
 		if (sscanf (buf, "%d", &nsr) == 1 && pr_) pr_->setSampleRateHW(nsr);
+		cfg_->set<C_SR,int>(nsr);
 		
 		return true;
 	}
--- airspy/gui_control.h
+++ airspy/gui_control.h
@@ -15,11 +15,15 @@
 #include "gui.h"
 #include "extio_config.h"
 #include "extio_airspy.h"
+#include <config.h>
+	
+typedef std::tuple<int, int, int, int, int, int, int> AIRSPY_CFG_T;
+	
 
 class AirSpyCtrlGui: public Gui {
 public:
 	AirSpyCtrlGui (ExtioAirSpyRadio < EXTIO_BASE_TYPE > *);
-	~AirSpyCtrlGui () {}
+	~AirSpyCtrlGui ();
 
 	void EnableControls();
 	void DisableControls();
@@ -31,6 +35,10 @@ public:
 
 private:
 	ExtioAirSpyRadio < EXTIO_BASE_TYPE > *pr_;
+	
+	enum { C_SR = 0, C_LNA_G = 1, C_MIX_G = 2, C_IF_G = 3, C_A_MIX_G = 4, C_A_LNA_G = 5, C_BIAS = 6 } ;
+	
+	Config<AIRSPY_CFG_T> *cfg_; // ("AIRSPY.txt", std::make_tuple(2500000, 5, 10, 6, 0, 0));
 };
 
 #endif
